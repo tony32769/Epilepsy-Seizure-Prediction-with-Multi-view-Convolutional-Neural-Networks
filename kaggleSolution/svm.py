71c45312dce94fbffbf95dbe0ad9f4539638793e
Bin Xiao <buptxiaofeng@gmail.com>
2016-12-24 17:24:46 +0800

    init

--- kaggleSolution/svm.py
+++ kaggleSolution/svm.py
@@ -9,20 +9,20 @@ from multiprocessing import Pool
 from sklearn import svm
 
 def run(setting):
-    feature = Feature(setting.name)
-    X_train, Y_train = feature.loadFromDisk("train","4D")
+    feature = Feature(setting.name, "kaggleSolution/kaggleSettings.yml")
+    X_train, Y_train = feature.loadFromDisk("4D", "train")
     X_train, Y_train = feature.overlapInEachHour()
     X_train, _ = feature.scaleAcrossTime(X_train)
-    X_test,  Y_test = feature.loadFromDisk("test","4D")
+    X_test,  Y_test = feature.loadFromDisk("4D", "test")
     X_test, _ = feature.scaleAcrossTime(X_test)
 
     channels = X_train.shape[1]
     bins = X_train.shape[2]
     steps = X_train.shape[3]
-    X_pca_train, Y_pca_train = feature.loadFromDisk("train","PCA")
+    X_pca_train, Y_pca_train = feature.loadFromDisk("PCA", "train")
     X_pca_train, Y_pca_train = feature.overlapInEachHour()
     X_pca_train, _ = feature.scaleAcrossTime(X_pca_train)
-    X_pca_test, Y_pca_test = feature.loadFromDisk("test","PCA")
+    X_pca_test, Y_pca_test = feature.loadFromDisk("PCA","test")
     X_pca_test, _ = feature.scaleAcrossTime(X_pca_test)
     X_train, X_pca_train, Y_train = feature.shuffle(X_train, X_pca_train, Y_train)
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2] * X_train.shape[3])
@@ -40,34 +40,19 @@ def run(setting):
     cv = StratifiedKFold(y, n_folds = 3, shuffle=True)
     clf = svm.SVC(probability = True, class_weight={1: zeros / ones})
 
-    mean_tpr = 0.0
-    mean_fpr = numpy.linspace(0,1,100)
-    for i, (train, test) in enumerate(cv):
-        clf.fit(X[train], y[train])
-        prob = clf.predict(X[test])
-        accuracy = accuracy_score(y[test], prob)
-        print "accuracy:" + str(accuracy)
-        fpr, tpr, thresholds = roc_curve(y[test], prob)
-        #TPR is also known as sensitivity
-        #FPR is one minus the specificity or true negative rate.
-        print "iteration:" + str(i)
-        print "fpr:" + str(fpr)
-        print "tpr:" + str(tpr)
-        print auc(fpr, tpr)
-        mean_tpr += interp(mean_fpr, fpr, tpr)
-        mean_tpr[0] = 0.0
-        roc_auc = auc(fpr, tpr)
-    mean_tpr /= len(cv)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    print "mean auc " + str(mean_auc)
     clf.fit(X,y)
     result = clf.predict_proba(X_test)
 
     output = result[:,1]
     ans = zip(Y_test,output)
     dataFrame = DataFrame(data=ans, columns=["clip", "preictal"])
-    dataFrame.to_csv(setting.savePath + featureName + ".csv", index=False, header = True)
+    dataFrame.to_csv(setting.savePath + setting.name + ".csv", index=False, header = True)
 featureName = ["Dog_1", "Dog_2", "Dog_3", "Dog_4", "Dog_5", "Patient_1", "Patient_2"]
-pool = Pool(7)
-pool.map(run, featureName)
+settingList = []
+for subject in featureName:
+    setting = Setting(path = "kaggleSolution/kaggleSettings.yml")
+    setting.loadSettings(subject)
+    settingList.append(setting)
+    run(setting)
+#pool = Pool(7)
+#pool.map(run, featureName)
