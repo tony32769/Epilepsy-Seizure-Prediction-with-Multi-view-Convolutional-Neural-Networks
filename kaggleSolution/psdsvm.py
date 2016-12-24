b1c79a4de61a3d5ee0b0babffc930f6f78dad8bf
Bin Xiao <buptxiaofeng@gmail.com>
2016-12-24 17:24:36 +0800

    init

--- kaggleSolution/psdsvm.py
+++ kaggleSolution/psdsvm.py
@@ -7,11 +7,11 @@ from pandas import DataFrame
 from multiprocessing import Pool
 from sklearn import svm
 
-def run(featureName):
-    feature = Feature(featureName)
-    X_train, Y_train = feature.loadFromDisk("train","psd")
-    X_train, Y_train = feature.overlapInEachHour()
-    X_test,  Y_test = feature.loadFromDisk("test","psd")
+def run(setting):
+    feature = Feature(setting.name, "kaggleSolution/kaggleSettings.yml")
+    X_train, Y_train = feature.loadFromDisk("psd", "train")
+    X_train, Y_train = feature.overlapInEachHour(shuffle = True)
+    X_test,  Y_test = feature.loadFromDisk("psd","test")
     X_test, _ = feature.scaleAcrossTime(X_test)
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2] * X_train.shape[3])
     X_test = X_test.reshape(X_test.shape[0],  X_test.shape[1] * X_test.shape[2] * X_test.shape[3])
@@ -30,8 +30,14 @@ def run(featureName):
     output = result[:,1]
     ans = zip(Y_test,output)
     dataFrame = DataFrame(data=ans, columns=["clip", "preictal"])
-    dataFrame.to_csv("/home/xiaobin/Disk/PSDSVMResult/" + featureName + ".csv", index=False, header = True)
+    dataFrame.to_csv("/home/xiaobin/Disk/" + setting.name + ".csv", index=False, header = True)
 
 featureName = ["Dog_1", "Dog_2", "Dog_3", "Dog_4", "Dog_5", "Patient_1", "Patient_2"]
-pool = Pool(7)
-pool.map(run, featureName)
+settingList = []
+for subject in featureName:
+    setting = Setting(path = "kaggleSolution/kaggleSettings.yml")
+    setting.loadSettings(subject)
+    settingList.append(setting)
+    run(setting)
+#pool = Pool(7)
+#pool.map(run, featureName)
